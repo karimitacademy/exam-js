4d579341eb54f5cf889b4b44a9f43cd6d8a50f08
karimitacademy <nkavkazka123@gmail.com>
2022-12-21 20:49:14 +0600

    calc

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+const vizual = document.querySelector('.box')
+
+
+
+
+const btn = document.getElementById('calc')
+
+const resultParagraph = document.getElementById('result')
+
+btn.addEventListener('click',() => {
+  const soms = document.getElementsByTagName('input')[0].value
+  const dollars = document.getElementsByTagName('input')[1].value
+
+const result = Number(soms) / Number(dollars)
+resultParagraph.innerText = result.toFixed(2)
+
+})
+
